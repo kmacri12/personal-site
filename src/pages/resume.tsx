b320a4ff623c4cdf384da6ec8435d71465feb28d
Kyle Macri <kmacri12@gmail.com>
2021-01-10 18:58:57 -0500

    Resume is showing

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "./resume.css";
+import { SizeMe } from "react-sizeme";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default class Resume extends Component {
   render() {
-    return <div>this is the resume pg!</div>;
+    return (
+      <div className="resume">
+        <SizeMe>
+          {({ size }) => (
+            <Document file="/KyleMacri_Resume.pdf" className="center">
+              <Page pageNumber={1} width={size.width ? size.width : 1} />
+            </Document>
+          )}
+        </SizeMe>
+      </div>
+    );
   }
 }
